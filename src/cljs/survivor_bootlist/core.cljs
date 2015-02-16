ced82f29c4ef1ceb8276cfee0ecf239bef13e062
Christopher Lopes <christopher.lopes@gmail.com>
2015-02-15 18:30:30 -0800

    Calculate points using seperate atom

--- src/cljs/survivor_bootlist/core.cljs
+++ src/cljs/survivor_bootlist/core.cljs
@@ -6,7 +6,7 @@
 ;; State
 
 (def contestants (atom [
-                        {:id 1 :name "Carolyn" :out 18 :tribe "masaya" :locked true}
+                        {:id 1 :name "Carolyn" :out 0 :tribe "masaya"}
                         {:id 2 :name "Dan" :out 0 :tribe "escameca"}
                         {:id 3 :name "Hali" :out 0 :tribe "nagarote"}
                         {:id 4 :name "Jenn" :out 0 :tribe "nagarote"}
@@ -26,14 +26,18 @@
                         {:id 18 :name "Will" :out 0 :tribe "nagarote"}
                         ]))
 
+(def voted-out-list (atom [
+                           {:id 1 :out 18}
+                           ]))
+
 (def entries (atom [
-                    {:id 1 :name "phil" :points 0 :order [18 17 16 15 14 13 12 11 1 10 9 8 7 6 5 4 3 2]}
-                    {:id 2 :name "will" :points 0 :order [2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 1]}
-                    {:id 3 :name "ciwchris" :points 0 :order [4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 1 2 3]}
-                    {:id 11 :name "last" :points 0 :order [3 1 2 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18]}
+                    ;;{:id 1 :name "phil" :points 0 :order [18 17 16 15 14 13 12 11 1 10 9 8 7 6 5 4 3 2]}
+                    ;;{:id 2 :name "will" :points 0 :order [2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 1]}
+                    ;;{:id 3 :name "ciwchris" :points 0 :order [4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 1 2 3]}
+                    {:id 4 :name "last" :points 0 :order [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18]}
                     ]))
 
-(def selected-entry (atom 1))
+(def selected-entry (atom 4))
 
 ;; -------------------------
 ;; To read the players when sorted
@@ -58,18 +62,18 @@
                          [:img {:src (str "/images/" (:name item) ".png")}]
                          [:div (:name item)]])])
 
-(defn create-contestant-list [entries selected-entry contestants]
-  (let [contestant-order (:order (first (filter #(= selected-entry (:id %)) entries)))]
-    (map
-     #(first (filter (fn [contestant] (= (:id contestant) %)) contestants))
-     contestant-order)))
+(defn create-contestant-list [entries selected-entry voted-out-list]
+  (let [entry-contestant-order (:order (first (filter #(= selected-entry (:id %)) entries)))]
+            (map
+             #(first (filter (fn [voted-out-entry] (= (:id voted-out-entry) %)) voted-out-list))
+             entry-contestant-order)))
 
 (defn calculate-points-for-entry
-  ([entry] (calculate-points-for-entry 0 18 entry ))
+  ([entry] (calculate-points-for-entry 0 18 entry))
   ([points max-points entry]
-   (if (<= max-points 0)
+   (if (empty? entry)
      points
-     (let [new-points (if (= 0 (:out (first entry)))
+     (let [new-points (if (nil? (first entry))
                         0
                         (Math/abs (- max-points (:out (first entry)))))]
        (recur
@@ -77,51 +81,53 @@
         (dec max-points)
         (rest entry))))))
 
+(defn calculate-points-for-entries [entries-to-modify voted-out-list]
+  (map #(assoc
+         %
+         :points
+         (calculate-points-for-entry (create-contestant-list entries-to-modify (:id %) voted-out-list)))
+       entries-to-modify))
+
+(defn sorted-entries [entries]
+   (into [] (sort #(compare (:points %2) (:points %1)) entries)))
+
 (defn leader-board [entries contestants]
-  [:ul
-   (for [entry entries]
-     ^{:key (:id entry)} [:li.ui-state-default 
-                          [:div {:class "points"} (:name entry)
-                           [:div (calculate-points-for-entry (create-contestant-list entries (:id entry) contestants))]]])])
+  [:div.column
+   [:h3 "Leaderboard"]
+   [:ul
+    (for [entry (sorted-entries entries)]
+      ^{:key (:id entry)} [:li.ui-state-default 
+                           [:div {:class "points"} (:name entry)
+                            [:div (:points entry)]]])]])
 
 (defn create-placed-list [placed]
   (into [] (sort #(compare (:out %2) (:out %1)) placed)))
 
-(defn create-contestant-list [entries selected-entry contestants]
-  (let [contestant-order (:order (first (filter #(= selected-entry (:id %)) entries)))]
-    (map
-     #(first (filter (fn [contestant] (= (:id contestant) %)) contestants))
-     contestant-order)))
-
 (defn entries-display [entries selected-entry]
-    [:div
-     [:select {:on-change #(swap! selected-entry (fn [] (int (-> % .-target .-value))))}
-      (for [entry entries]
-        ^{:key (:id entry)} [:option {:value (:id entry)}
-                             (:name entry)])]
-     [lister (create-contestant-list entries @selected-entry @contestants) "selected-entry"]])
-
+  [:div.column
+   [:h3 "Entries"]
+   [:select {:on-change #(swap! selected-entry (fn [] (int (-> % .-target .-value))))}
+    (for [entry entries]
+      ^{:key (:id entry)} [:option {:value (:id entry)}
+                           (:name entry)])]
+   [lister (create-contestant-list entries @selected-entry @contestants) "selected-entry"]])
 
 (defn home []
-  [:div [:h2 "Phil & Will's Survivor 30 Bootlist"]
-   [leader-board @entries @contestants]
-   [entries-display @entries selected-entry]
-   [lister (create-placed-list  (filter #(true? (:locked %)) @contestants)) "placed"]
-   [lister (filter #(= 0 (:out %)) @contestants) "contestants"]])
+  (let [entries (calculate-points-for-entries @entries @voted-out-list)]
+    [:div [:h2 "Phil & Will's Survivor 30 Bootlist"]
+     [leader-board entries @contestants]
+     [entries-display entries selected-entry]
+     [:div.column
+      [:h3 "Voted out"]
+      [lister (create-placed-list  (filter #(true? (:locked %)) @contestants)) "placed"]]
+     [:div.column
+      [:h3 "Active"]
+      [lister (filter #(= 0 (:out %)) @contestants) "contestants"]]]))
 
-(defn update-c [cs ids]
-  (if (empty? ids)
-    cs
-    (let [c (first (filter #(= (:id (first ids)) (:id %)) cs))
-          new-cs (remove #(= (:id (first ids)) (:id %)) cs)
-          new-c (assoc c :out (:out (first ids)))]
-      (recur
-       (into [] (conj new-cs new-c))
-       (rest ids)))))
 
-(defn re-sort [new-placed-list]
-  (let [ids (map-indexed (fn [index item] (hash-map :id (int (.-id item)) :out (- 18 index))) new-placed-list)]
-    (swap! contestants (fn [] (update-c @contestants (into [] ids))))))
+(defn update-voted-out [new-placed-list]
+  (let [new-sorted-list (map-indexed (fn [index item] (hash-map :id (int (.-id item)) :out (- 18 index))) new-placed-list)]
+    (swap! voted-out-list (fn [] new-sorted-list))))
 
 (defn home-did-mount []
   (js/$ (fn []
@@ -132,7 +138,7 @@
           (.sortable (js/$ "#placed") (clj->js {
                                                 :items "li:not(.ui-state-disabled)"
                                                 :connectWith ".connectedSortable"
-                                                :update (fn [e ui] (re-sort (-> e .-target .-childNodes)))
+                                                :update (fn [e ui] (update-voted-out (-> e .-target .-childNodes)))
                                                 }))
           (.disableSelection (js/$ "#placed")))))
 
